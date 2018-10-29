900867a94e4736f0c6a384c1cd6f181f9d067d16
heha-proglea <heha965+proglea@gmail.com>
2018-10-29 04:17:57 +0000

    Add comments

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const http = require('http');
 const auth = require('http-auth');
 const router = require('./lib/router');
 
+// Basic認証の設定
 const basic = auth.basic({
+  // basic認証で覆う(保護する)範囲の指定
   realm: 'Enter username and password.',
   file: './users.htpasswd'
 });
 
+// 第一引数にbasicオブジェクトを渡すことで、basic認証に対応
 const server = http.createServer(basic, (req, res) => {
   router.route(req, res);
 }).on('error', (e) => {
